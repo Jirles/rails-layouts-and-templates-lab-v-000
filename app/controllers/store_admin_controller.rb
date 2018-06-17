0e1242533c43f9d203091bcb1ea59740042df8b1
Emily Jirles <emily.jirles@gmail.com>
2018-06-17 21:40:58 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,10 +1,10 @@
 class StoreAdminController < ApplicationController
   layout 'admin'
-
+  
   def home
   end
 
-  def orders
-
+  def orders 
+    render layout: "order_administration"
   end 
 end
